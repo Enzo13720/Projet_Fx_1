7bcd2ff310711c28d980b45a93cfc441a27c16d0
COMBELONGE Enzo <c22020966@A-F002097.salsa.univ-amu.fr>
2023-05-15 14:06:06 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.projet_fx1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.projet_fx1 to javafx.fxml;
+    exports com.example.projet_fx1;
+}
